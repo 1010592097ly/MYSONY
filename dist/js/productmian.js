81eadd6b983e7e7149ea42338cd588a68c632da2
1010592097ly <1010592097@qq.com>
2020-07-29 10:56:22 +0800

    商品详情页加入导航栏

--- dist/js/productmian.js
+++ dist/js/productmian.js
@@ -3,19 +3,21 @@ require.config({
     paths:{
         jquery:"jquery-1.11.3",
         "jquery-cookie": "jquery.cookie",
-        product:"product"
+        product:"product",
 
     },
     shim: {
         //设置依赖关系
         "jquery-cookie": ["jquery"],
-        //某一个模块，不遵从AMD
-        parabola: {
-          exports: "_",
-        },
       },
 })
 require(["product"], function (product) {
     product.productload();
     product.lookgoodsClick();
+    product.download();
+    product.main();
+    product.webload();
+    product.serveload();
+    product.mysonyload();
+
 })
